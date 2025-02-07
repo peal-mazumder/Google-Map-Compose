444fc3dd54b34d8c8125f7184f4da7cc62932576
Peal Mazumder <peal.mazumder@brainstation-23.com>
2025-02-07 19:40:38 +0600

    Add advance marker

--- app/src/main/java/com/peal/googlemapjetpackcompose/presentation/AdvancedMarkersMapContent.kt
+++ app/src/main/java/com/peal/googlemapjetpackcompose/presentation/AdvancedMarkersMapContent.kt
@@ -14,21 +14,77 @@
 
 package com.peal.googlemapjetpackcompose.presentation
 
+
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.peal.googlemapjetpackcompose.presentation.MountainsScreenViewState.MountainList
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.model.AdvancedMarkerOptions
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.PinConfig
+import com.google.maps.android.compose.AdvancedMarker
+import com.google.maps.android.compose.GoogleMapComposable
+import com.google.maps.android.compose.rememberMarkerState
+import com.peal.googlemapjetpackcompose.R
 import com.peal.googlemapjetpackcompose.data.local.Mountain
+import com.peal.googlemapjetpackcompose.data.local.is14er
+import com.peal.googlemapjetpackcompose.data.utils.toElevationString
+import com.peal.googlemapjetpackcompose.presentation.utils.BitmapParameters
+import com.peal.googlemapjetpackcompose.presentation.utils.vectorToBitmap
+
 
 /**
  * [GoogleMapComposable] which renders a [MountainList] as a set of [AdvancedMarker]s
  */
-// TODO: Add @GoogleMapComposable annotation
+
 @Composable
+@GoogleMapComposable
 fun AdvancedMarkersMapContent(
     mountains: List<Mountain>,
     onMountainClick: (Marker) -> Boolean = { false },
 ) {
-    // TODO: Create custom [PinConfig]s for fourteeners and for other mountains
+    val mountainIcon = vectorToBitmap(
+        LocalContext.current,
+        BitmapParameters(
+            id = R.drawable.baseline_filter_hdr_24,
+            iconColor = MaterialTheme.colorScheme.onSecondary.toArgb(),
+        )
+    )
 
-    // TODO: Create AdvancedMarkers from each of the mountains
+    val mountainPin = with(PinConfig.builder()) {
+        setGlyph(PinConfig.Glyph(mountainIcon))
+        setBackgroundColor(MaterialTheme.colorScheme.secondary.toArgb())
+        setBorderColor(MaterialTheme.colorScheme.onSecondary.toArgb())
+        build()
+    }
+
+    val fourteenerIcon = vectorToBitmap(
+        LocalContext.current,
+        BitmapParameters(
+            id = R.drawable.baseline_filter_hdr_24,
+            iconColor = MaterialTheme.colorScheme.onPrimary.toArgb(),
+        )
+    )
+
+    val fourteenerPin = with(PinConfig.builder()) {
+        setGlyph(PinConfig.Glyph(fourteenerIcon))
+        setBackgroundColor(MaterialTheme.colorScheme.primary.toArgb())
+        setBorderColor(MaterialTheme.colorScheme.onPrimary.toArgb())
+        build()
+    }
+
+    mountains.forEach { mountain ->
+        val pin = if (mountain.is14er()) fourteenerPin else mountainPin
+        AdvancedMarker(
+            state = rememberMarkerState(position = mountain.location),
+            title = mountain.name,
+            snippet = mountain.elevation.toElevationString(),
+            collisionBehavior = AdvancedMarkerOptions.CollisionBehavior.REQUIRED_AND_HIDES_OPTIONAL,
+            pinConfig = pin,
+            onClick = { marker ->
+                onMountainClick(marker)
+                false
+            }
+        )
+    }
 }
